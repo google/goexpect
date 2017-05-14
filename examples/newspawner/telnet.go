26291884b5b9e58b7853e8864ff61504c57be856
Ola Karlsson <olakar@google.com>
2017-05-14 10:31:09 +1000

    Could this possibly work?

--- examples/newspawner/telnet.go
+++ examples/newspawner/telnet.go
@@ -1,23 +1,44 @@
-// telnet crates a new Expect sp
+// telnet crates a new Expect spawner for Telnet.
 package main
 
 import (
 	"fmt"
-	"net"
 	"time"
 
 	expect "github.com/google/goexpect"
+
+	"github.com/golang/glog"
 	"github.com/google/goterm/term"
 	"github.com/ziutek/telnet"
 )
 
+const (
+	network = "tcp"
+	address = "telehack.com:23"
+	timeout = 10 * time.Second
+	command = "geoip"
+)
+
 func main() {
 	fmt.Println(term.Bluef("Telnet spawner example"))
+	exp, _, err := telnetSpawn(address, timeout)
+	if err != nil {
+		glog.Exitf("telnetSpawn(%q,%v) failed: %v", address, timeout, err)
+	}
+	res, err := exp.ExpectBatch([]expect.Batcher{
+		&expect.BExp{R: `\n\.`},
+		&expect.BSnd{S: command + "\n"},
+		&expect.BExp{R: `\n\.`},
+	}, timeout)
+	if err != nil {
+		glog.Exit(err)
+	}
+	fmt.Println(term.Greenf("Res: %s", res[len(res)-1].Output))
 
 }
 
-func telnetSpawn(network string, addr net.IP, timeout time.Duration) (expect.Expecter, <-chan error, error) {
-	conn, err := telnet.Dial(network, addr.String())
+func telnetSpawn(addr string, timeout time.Duration) (expect.Expecter, <-chan error, error) {
+	conn, err := telnet.Dial(network, addr)
 	if err != nil {
 		return nil, nil, err
 	}
