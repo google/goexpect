2aabd439007e5a957762f377498b0886e75787f9
Ysu丶hk <827435393@qq.com>
2020-07-03 18:07:23 +0800

    reset timer when new data is available.
    timer should means that, there was no new out put data in the past specific secdons. So timer shoud be reset when new out put is available.

--- expect.go
+++ expect.go
@@ -800,13 +800,13 @@ func (e *GExpect) ExpectSwitchCase(cs []Caser, timeout time.Duration) (string, [
 		case <-e.rcv:
 			// Data to fetch.
 			nr, err := io.Copy(&tbuf, e)
-            if err != nil {
+		    	if err != nil {
 				return tbuf.String(), nil, -1, fmt.Errorf("io.Copy failed: %v", err)
 			}
-            // now should reset timer??
-            if nr > 0 {
-                timer = time.NewTimer(timeout)
-            }
+			// timer shoud be reset when new out put is available.
+			if nr > 0 {
+				timer = time.NewTimer(timeout)
+			}
 		}
 	}
 }
